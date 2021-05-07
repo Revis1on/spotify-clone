6033161853509090c4a80576b793ab3281be7468
Filip Risteski <filip22risteski@gmail.com>
2021-05-07 23:51:07 +0200

    v1.0

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,21 +1,35 @@
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import React from "react";
+import { useDataLayerValue } from "../../DataLayer/DataLayer";
 import "./Body.css";
 import Header from "./Components/Header";
+import SongRow from "./Components/SongRow";
 
 function Body({ spotify }) {
+  const [{ discover_weekly }, dispatch] = useDataLayerValue();
+
   return (
     <div className="body">
       <Header spotify={spotify} />
       <div className="body__info">
-        <img
-          src="https://www.hypebot.com/wp-content/uploads/2020/07/discover-weekly.png"
-          alt=""
-        />
+        <img src={discover_weekly?.images[0].url} alt="" />
         <div className="body__infoText">
           <strong>PLAYLIST</strong>
           <h2>Discover Weekly</h2>
-          <p>description...</p>
+          <p>{discover_weekly?.description}</p>
         </div>
+      </div>
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon className="body__shuffle" />
+          <FavoriteIcon fontSize="large" />
+          <MoreHorizIcon />
+        </div>
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow track={item.track} />
+        ))}
       </div>
     </div>
   );
